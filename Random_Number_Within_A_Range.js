6e0a48b0f75d1a9050c6b2710cca8095a27d6c6d
Kennedy Jim <110538782+Kennedy01-crypto@users.noreply.github.com>
2023-05-15 14:05:00 +0300

    Random Number Generation Within a Given Range
    The JavaScript Program Generates A random Number Within a given range.

---
+++ Random_Number_Within_A_Range.js
@@ -0,0 +1,10 @@
+//program to output the random numbers in a given range
+let rangeStart = prompt("Enter lowest number;")
+let rangeEnd = prompt("Enter largest number;")
+
+function randomNumRange(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+} 
+console.log(randomNumRange(rangeStart,rangeEnd));
