f8c9d894740c6020e441d32cb06863bf6d8f1783
Kennedy Jim <110538782+Kennedy01-crypto@users.noreply.github.com>
2023-05-19 15:08:01 +0300

    Update ReverseANumber.js
    Simplified the function. The previous code has been commented out

--- ReverseANumber.js
+++ ReverseANumber.js
@@ -1,12 +1,14 @@
 function MyReverseFunction(num) {
-    var reverse = 0;
-    while(num != 0){
-        reverse = reverse * 10 ;
-        reverse = reverse + num%10;
-        //to remove the decimal place
-        num = Math.trunc(num/10);
-    }
-    return reverse;
+//     var reverse = 0;
+//     while(num != 0){
+//         reverse = reverse * 10 ;
+//         reverse = reverse + num%10;
+//         //to remove the decimal place
+//         num = Math.trunc(num/10);
+//     }
+//     return reverse;
+    num = num + "";
+	return num.split("").reverse().join("");
 }
 console.log(MyReverseFunction(123)); //321
 console.log(MyReverseFunction(12345678)); //87654321
