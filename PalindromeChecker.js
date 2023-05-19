9803d7886103ba0aed5da277d6178de4aa6e2992
Kennedy Jim <110538782+Kennedy01-crypto@users.noreply.github.com>
2023-05-19 15:47:28 +0300

    Create PalindromeChecker.js
    This program checks whether the word passed is a Palindrome

---
+++ PalindromeChecker.js
@@ -0,0 +1,23 @@
+//A function that checks whether a passed String is a Palindrome or not
+function IsPalindrome(word){
+    if(typeof word == "string"){
+      let word2 = word.toLowerCase();
+      let word3 = word2.split("").reverse("").join("");
+
+      if(word3 === word2){
+        return `TRUE: The word ${word} is a Palindrome`;
+      }
+      else(word3 !== word2);{
+        return `FALSE: The word ${word} is NOT a Palindrome`;
+      }
+    }
+    else{
+      return "Error! NOT a string";
+    }  
+  }  
+  //call the function
+  console.log(IsPalindrome("madam"));
+  console.log(IsPalindrome("MmadaMm"));
+  console.log(IsPalindrome("Joy"));
+  console.log(IsPalindrome("11eeye"));
+  console.log(IsPalindrome("otto"));
